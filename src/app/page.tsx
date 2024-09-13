6c11303708d575d7ff32d987a4dd22c975017377
Nelson Felipe <nelsonfelipeandradearaujo.@gmail.com>
2024-09-13 10:58:21 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,83 @@
-import Image from "next/image";
+"use client";
+
+import { useChat } from "ai/react";
+import Markdown from "react-markdown";
+// import Robo from "/public/robo.png";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { messages, input, handleInputChange, handleSubmit, isLoading } =
+    useChat();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="grid grid-rows-[min-content_1fr_min-content] bg-zinc-800 w-[440px] h-[600px] rounded-lg p-5">
+        <h1 className="text-lg mb-6 text-center">AI Chatbot</h1>
+        {messages.length === 0 ? (
+          <div className="flex flex-col items-center mt-6 gap-2">
+            {/*             <Image
+              src={Robo}
+              className="rounded-full"
+              width={80}
+              height={80}
+              alt="Chatbot"
+            /> */}
+            <p>Como posso te ajudar?</p>
+          </div>
+        ) : (
+          <div className="max-h-[556px] space-y-5 overflow-y-auto [&::-webkit-scrollbar]:hidden">
+            {messages.map((message) => {
+              return (
+                <div
+                  key={message.id}
+                  className="flex gap-3 mb-2 text-slate-50 text-sm"
+                >
+                  {message.role === "user" && (
+                    <div className="flex-shrink-0">
+                      <p>imagem do usuario</p>
+                      {/* <Image
+                        src={Gatinho}
+                        className="rounded-full"
+                        width={40}
+                        height={40}
+                        alt="user"
+                      /> */}
+                    </div>
+                  )}
+                  {message.role === "assistant" && (
+                    <div className="flex-shrink-0">
+                      {/* <Image
+                        src={Robo}
+                        className="rounded-full"
+                        width={40}
+                        height={40}
+                        alt="Chatbot"
+                      /> */}
+                    </div>
+                  )}
+                  <p className="leading-relaxed mb-4 mr-3">
+                    <span className="block font-bold text-slate-50">
+                      {message.role === "user" ? "Usuário" : "AI"}
+                    </span>
+                    <Markdown>{message.content}</Markdown>
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        <form onSubmit={handleSubmit} className="flex justify-center gap-2">
+          <input
+            disabled={isLoading}
+            className="text-black p-2 rounded w-full"
+            value={input}
+            placeholder="Faça uma pergunta"
+            onChange={handleInputChange}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <button type="submit" className="bg-blue-500 px-3 py-2 rounded">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
